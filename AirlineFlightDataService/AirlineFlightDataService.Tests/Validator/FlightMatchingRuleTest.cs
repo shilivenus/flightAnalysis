2e3a441b2916a91b9ecae4e161befa53e2524205
Peter Shi <Peter.Shi@enett.com>
2019-04-15 16:56:12 +1000

    first unit test setup

---
+++ AirlineFlightDataService/AirlineFlightDataService.Tests/Validator/FlightMatchingRuleTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AirlineFlightDataService.Module;
+using AirlineFlightDataService.Validator.Rules;
+using AutoFixture;
+using Xunit;
+
+namespace AirlineFlightDataService.Tests.Validator
+{
+    public class FlightMatchingRuleTest
+    {
+        [Fact]
+        public void ReturnTrueWhenFlightIsNotNull()
+        {
+            //Arrange
+            var fixture = new Fixture();
+
+            var flightEvent = fixture.Build<Event>()
+                .With(x => x.Flight, "AAV732")
+                .Create();
+
+            var flightMatchingRule = new FlightMatchingRule();
+
+            //Act
+            var result = flightMatchingRule.IsMatched(flightEvent);
+
+            //Assert
+            Assert.True(result);
+        }
+    }
+}
